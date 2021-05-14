25222d384bc0b2daeb4a49e4e190cff8f7248f84
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-14 22:39:27 +0300

    configure react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import {PageNotFound} from "./PageNotFound/";
 import {People} from "./People";
 
 function App() {
   return (
-    <People/>
+    <Router>
+      <Switch>
+        <Route path="/" component={People} exact />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
+    </Router>
   );
 }
 
