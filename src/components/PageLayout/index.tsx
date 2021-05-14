8e74714bd839434406f2ce5b920b029ec2ca71db
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-14 22:39:04 +0300

    create a reusable PageLayout

---
+++ src/components/PageLayout/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import {Container} from "./styles";
+
+export const PageLayout: React.FC<{}> = ({children}) => (
+	<Container>
+		{children}
+	</Container>
+);
---
+++ src/components/PageLayout/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	background: #F9FAFB;
+	height: 100vh;
+`;
+
