b6d992c6092cd70018f484041962b6de66098f90
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-15 13:43:01 +0300

    add PersonDetails route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import {PageNotFound} from "./PageNotFound/";
 import {People} from "./People";
+import {PersonDetails} from "./PersonDetails";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={People} exact />
+        <Route path="/person_details"><PersonDetails/></Route>
         <Route path="*" component={PageNotFound} />
       </Switch>
     </Router>
