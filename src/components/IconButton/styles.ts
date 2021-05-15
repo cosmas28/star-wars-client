7f2aca1429759822264ab6f506e8d5233eecb0d0
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-15 11:29:42 +0300

    create IconButton

---
+++ src/components/IconButton/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+	border-radius: 50px;
+	border: 1px solid #2097F9;
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	align-self: stretch;
+	background: none;
+	color: #2097F9;
+	width: 48px;
+	height: 48px;
+	font-size: 20px;
+	cursor: pointer;
+
+	&:hover {
+		color: #055EA8;
+		border-color: #055EA8;
+	}
+
+	&:focus {
+		outline: 0;
+	}
+`;
