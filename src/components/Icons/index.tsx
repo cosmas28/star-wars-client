5c94d4c4a325b349af2ba603773abb3bc98b89ad
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-15 11:30:11 +0300

    add chevronLeft icon

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,10 +1,11 @@
-import {BsChevronRight, BsHouse, BsSearch} from "react-icons/bs";
+import {BsChevronRight, BsChevronLeft, BsHouse, BsSearch} from "react-icons/bs";
 
-export type IconName = "chevronRight" | "home" | "search";
+export type IconName = "chevronRight" | "home" | "search" | "chevronLeft";
 export type IconType = {[name in IconName]: JSX.Element}
 
 export const ICONS: IconType = {
 	chevronRight: <BsChevronRight />,
+	chevronLeft: <BsChevronLeft/>,
 	home: <BsHouse />,
 	search: <BsSearch />,
 };
