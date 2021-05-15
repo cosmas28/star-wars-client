9ede1ea5e59be99f26a3c7026dadb00c6e38912a
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-16 01:22:33 +0300

    add ellipsi icon

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,6 +1,6 @@
-import {BsChevronRight, BsChevronLeft, BsHouse, BsSearch, BsStar, BsExclamationTriangle} from "react-icons/bs";
+import {BsChevronRight, BsChevronLeft, BsHouse, BsSearch, BsStar, BsExclamationTriangle, BsThreeDots} from "react-icons/bs";
 
-export type IconName = "chevronRight" | "home" | "search" | "chevronLeft" | "star" | "exclamationTriangle";
+export type IconName = "chevronRight" | "home" | "search" | "chevronLeft" | "star" | "exclamationTriangle" | "ellipsis";
 export type IconType = {[name in IconName]: JSX.Element}
 
 export const ICONS: IconType = {
@@ -10,4 +10,5 @@ export const ICONS: IconType = {
 	search: <BsSearch />,
 	star: <BsStar/>,
 	exclamationTriangle: <BsExclamationTriangle />,
+	ellipsis: <BsThreeDots /> 
 };
