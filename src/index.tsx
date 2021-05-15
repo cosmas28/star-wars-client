96148fb6c8d6e9cc0341d93a52889e10a447788e
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-15 21:51:36 +0300

    setup apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  gql,
+  NormalizedCacheObject,
+  InMemoryCache,
+} from "@apollo/client";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const cache: InMemoryCache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        peopleData: {
+          merge: true,
+        },
+        person: {
+          merge: true,
+        }
+      }
+    }
+  }
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: "http://localhost:8000/graphql"
+});
 
 ReactDOM.render(
   <React.StrictMode>
