adddffc45457e51190b69bd6948e66e093353189
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-16 11:20:49 +0300

    add graphql queries to fetch person details

--- src/PersonDetails/index.tsx
+++ src/PersonDetails/index.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
+import {useParams, useHistory} from "react-router-dom";
+import {gql, useQuery} from "@apollo/client";
 
 import {Avatar} from "../components/Avatar";
 import {DetailsRow} from "../components/DetailsRow";
 import {DetailsPageLayout} from "../components/DetailsPageLayout";
+import {NotFound} from "../components/NotFound";
+import {Snackbar} from "../components/Snackbar";
 
 import {DotDivider} from "../components/PersonCard/styles"
 import {
@@ -13,27 +17,84 @@ import {
 	HeaderTitleWrapper
 } from "./styles";
 
+export const GET_PERSON_DETAILS_NAME = gql`
+	query GetPersonDetails($name: String!) {
+		person(name: $name) {
+			name
+			height 
+			mass
+			hairColor 
+			skinColor
+			eyeColor
+			birthYear
+			gender
+			homeworld
+			films
+			species
+			vehicles
+			starships
+			created
+			edited
+			url
+		}
+	}
+`
+
 export const PersonDetails: React.FC<{}> = () => {
+	const history = useHistory();
+	const {name} = useParams<{name: string}>();
+	const {loading, error, data} = useQuery(GET_PERSON_DETAILS_NAME, {
+		variables: {
+			name,
+		}
+	})
+
 	return (
 		<DetailsPageLayout
-			handleBackButtonClick={() => console.log("TO-DO. Back to home page")}
+			loading={loading}
+			handleBackButtonClick={() => history.push("/")}
 			renderHeader={() => (
 				<>
-					<AvatarWrapper><Avatar sizeInPx="56" initialLetter="L" /></AvatarWrapper>
+					<AvatarWrapper>
+						<Avatar
+							sizeInPx="56"
+							initialLetter={data?.person?.name ? data.person.name.substr(0, 1).toUpperCase() : ("A")}
+						/>
+					</AvatarWrapper>
 					<HeaderTitleWrapper>
-						<NameWrapper>Leia Organa</NameWrapper>
+						<NameWrapper>{data?.person.name}</NameWrapper>
 						<DotDivider/>
 						<HomeworldLink>Home world</HomeworldLink>
 					</HeaderTitleWrapper>
 				</>
 			)}
 		>
-			<Divider/>
-			<DetailsRow
-				label="Height"
-				value="150 cm"
-			/>
-			<Divider/>
+			{
+				data?.person ? (
+					<>
+						<Divider/>
+						{
+							Object.keys(data?.person).map((key, index) => (
+								<React.Fragment key={index}>
+									<DetailsRow
+										label={key}
+										value={data.person[key]}
+									/>
+									<Divider/>
+								</React.Fragment>
+							))
+						}
+					</>
+					) : (
+						<NotFound
+							iconName="search"
+							message="No results found"
+							handleClick={() => console.log("TO-DO: Go to home page!")}
+							buttonLabel="Go home"
+						/>
+					)
+			}
+		<Snackbar active={!!error} >Something seems to wrong with the system. Please try again later!</Snackbar>
 		</DetailsPageLayout>
 	)
 }
