95fc4fedc9eb3061360b7de6439deed80c0fc2d3
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-17 13:17:36 +0300

    refine App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { InMemoryCache } from '@apollo/client';
+
+import { renderApollo } from './test-utils';
+import { GET_PEOPLE_DATA } from './pages/People';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  const cache = new InMemoryCache({ addTypename: false });
+
+  describe('with data', () => {
+    const mocks = [
+      {
+        request: {
+          query: GET_PEOPLE_DATA,
+          variables: {
+            page: 1,
+            search: '',
+          },
+        },
+        result: {},
+        error: new Error(),
+      },
+    ];
+
+    it('renders app without error', () => {
+      renderApollo(<App />, { mocks, cache });
+    });
+  });
 });
