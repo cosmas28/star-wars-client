c64f4a130412cd7871f9f50774c89260569e2521
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-17 09:33:34 +0300

    write test for DetailsRow

---
+++ src/components/DetailsRow/DetailsRow.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render, cleanup } from '../../test-utils';
+
+import { DetailsRow } from './';
+
+describe('DetailsRow', () => {
+  afterEach(cleanup);
+
+  it('renders without error', () => {
+    render(<DetailsRow label="Mass" value="64 kg" />);
+  });
+});
